16269f4eaae4d19519704c9fda5a829ad4c76dc6
h2410745 <hai-long.feng@mail.foxconn.com>
2019-05-06 16:02:56 +0800

    测试代码

---
+++ XiaoShiZi/src/main/java/edu/children/xiaoshizi/activity/ConstraintActivity.java
@@ -0,0 +1,15 @@
+package edu.children.xiaoshizi.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.children.xiaoshizi.R;
+
+public class ConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint);
+    }
+}
